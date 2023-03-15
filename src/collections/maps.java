c21ad479bc0fa12f9f18abffb82d7e698836123f
Edwaldo Almeida <edwaldoalmeida@icloud.com>
2023-03-14 20:54:15 -0400

    Improving comments and adding iteration/access via Iterators

--- src/collections/maps.java
+++ src/collections/maps.java
@@ -9,13 +9,14 @@ public class maps {
         // creating
         Map<String, String> myHashMap = new HashMap<>();
         Map<String, String> myLinkedHasMap = new LinkedHashMap<>();  // preserves insertion or access order
-        Map<String, String> myTreeMap = new TreeMap<>();  // allows ordering
+        
+        // inserting one-by-one
         myHashMap.put("I", "eu");
         myHashMap.put("nothing", "nada");
 
         Integer size = myHashMap.size();
 
-        // inserting
+        // inserting from lists
         String[] wordsEng = {"this", "is", "a", "test"};
         String[] wordsPt = {"isto", "eh", "um", "teste"};
         for (int i = 0; i < wordsEng.length; i++) {
@@ -23,31 +24,32 @@ public class maps {
         }
         size = myHashMap.size();
 
-        // TODO: move from myHashMap to myTreeMap
-
-        // accessing the map
+        // iterating / accessing the map (regular/old method)
         for (int i = 0; i < wordsEng.length; i++) {
             String t = myHashMap.get(wordsEng[i]);  // gets the value corresponding to that key
             System.out.println(t);
         }
 
+        // checking if a key or value exist
         boolean result1 = myHashMap.containsKey(wordsEng[0]);  // true
         boolean result2 = myHashMap.containsKey(wordsEng[0].toUpperCase());  // false
         boolean result3 = myHashMap.containsValue("isto");  // true
 
+        // creating another datastructures with the Map content
         Set<String> keys = myHashMap.keySet();
         Collection<String> values = myHashMap.values();
         Set<Map.Entry<String,String>> entries = myHashMap.entrySet();
         System.out.printf("Map: %s%nKeys: %s%nValues: %s%nEntries: %s%n",
                             myHashMap, keys, values, entries);
 
+        // iterating / accessing the map (like Python's 'for')
         for(String key : myHashMap.keySet()) {
-            String k = key;
-           System.out.println(k);
+            // String k = key;
+            System.out.println(key);
         }
         for(String value : myHashMap.values()) {
-            String v = value;
-           System.out.println(v);
+            // String v = value;
+            System.out.println(value);
         }
         for(Map.Entry<String,String> pair : myHashMap.entrySet()) {
             String k = pair.getKey();
@@ -63,6 +65,22 @@ public class maps {
         boolean isEmpty = myHashMap.isEmpty();  // false
         myHashMap.clear();
         isEmpty = myHashMap.isEmpty();  // true
+
+        // creating a TreeMap
+        Map<String, Integer> myTreeMap = new TreeMap<>();  // allows ordering
+
+        // inserting data
+        String[] tNames = {"one", "two", "three", "four"};
+        Integer[] tNumbers = {1, 2, 3, 4, 5};
+        for (int i = 0; i < tNames.length; i++) {
+            myTreeMap.put(tNames[i], tNumbers[i]);
+        }
+
+        // iterating (using Iterators)
+        Iterator<Map.Entry<String, Integer>> iterator = myTreeMap.entrySet().iterator();
+        Map.Entry<String, Integer> iAmHere = iterator.next();
+        Map.Entry<String, Integer> youAreTheNextOne = iterator.next();
+
 
     }
 }
