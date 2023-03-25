c19ecc792e6f1d68afdf64fb2b6ddaf571754a7f
Edwaldo Almeida <edwaldoalmeida@icloud.com>
2023-03-24 20:52:05 -0400

    improving the FlowControl stuff

--- src/main/java/com/edwaldoalmeida/javalivesummary/FlowControl.java
+++ src/main/java/com/edwaldoalmeida/javalivesummary/FlowControl.java
@@ -2,26 +2,31 @@ package com.edwaldoalmeida.javalivesummary;
 
 public class FlowControl {
     public static void main(String[] args) {
+
         // if
-        boolean loop = true;
-        if (loop) {
-            // do something
+        int loop = 0;
+        if (loop >= 0) {
+            System.out.println("The variable `loop` is greater or equals to 0, so I was printed.");
+        } else if (loop < 0) {
+            System.out.println("The variable loop is less than 0, so I was printed.");
         } else {
-            // do something else
-        } // there is no `finally` in Java
+            System.out.println("That is never going to happen.");
+        }
 
         // while
         int i = 0;
         while (i < 5) {
-            // do something
+            System.out.println("I'm inside a while loop and this is the iteration number:" + i );
             i += 1;
         }
 
+
         // do-while
+        int j = 0;
         do {
-            // do something
-            i += 1;
-        } while (i < 5);
+            System.out.println("I'm inside a do-while loop and this is the iteration number:" + j );
+            j += 1;
+        } while (j < 5);
 
         // for ("traditional")
 
