0786d6839c9d3277328bd738332763ee1c25e53b
Edwaldo Almeida <edwaldoalmeida@icloud.com>
2023-04-03 21:42:32 -0400

    adding FileHandling, initially read and print

---
+++ src/main/java/com/edwaldoalmeida/javalivesummary/filehandling/FileHandling.java
@@ -0,0 +1,22 @@
+package com.edwaldoalmeida.javalivesummary.filehandling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileHandling {
+    public static void main(String[] args) {
+        String fileName = "test.txt";
+        
+        // reading and printing to the console
+        FileReader fileReader = new FileReader(fileName);
+        try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println("Line: " + line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error handling file: " + e.getMessage());
+        }
+    }
+}
