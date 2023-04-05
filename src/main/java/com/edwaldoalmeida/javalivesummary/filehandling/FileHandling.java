35c0db2339d653de620d3292895e7ab9ac27287a
Edwaldo Almeida <edwaldoalmeida@icloud.com>
2023-04-04 20:23:42 -0400

    improving notes/solution to show File Handling

--- src/main/java/com/edwaldoalmeida/javalivesummary/filehandling/FileHandling.java
+++ src/main/java/com/edwaldoalmeida/javalivesummary/filehandling/FileHandling.java
@@ -1,15 +1,17 @@
 package com.edwaldoalmeida.javalivesummary.filehandling;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class FileHandling {
     public static void main(String[] args) {
-        String fileName = "test.txt";
-        
+        String fileName = "README.md";
+        String absolutePath = new File(fileName).getAbsolutePath();
+
         // reading and printing to the console
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(absolutePath))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 System.out.println("Line: " + line);
