2343c32a8815a5f0f9c5977f21df3f46c4aed932
Edwaldo Almeida <edwaldoalmeida@icloud.com>
2023-08-08 20:50:41 -0400

    pushing new Arrays notes

---
+++ src/main/java/com/edwaldoalmeida/javalivesummary/Arrayz.java
@@ -0,0 +1,14 @@
+package com.edwaldoalmeida.javalivesummary;
+
+import java.util.Arrays;
+
+public class Arrayz {
+    public static void main(String[] args) {
+        int[] Array1 = new int[2];
+        int[] Array2 = new int[2];
+        Array1[0] = 10;
+        Array2[1] = 20;
+        System.out.println(Array1.length);
+        System.out.println(Arrays.toString(Array2));
+    }
+}
