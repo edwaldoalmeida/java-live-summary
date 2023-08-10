0c4538982f436164f215eaaeb15ab4eb00d4fe97
Edwaldo Almeida <edwaldoalmeida@icloud.com>
2023-08-09 23:07:41 -0400

    includes new file: Stringz.java

---
+++ src/main/java/com/edwaldoalmeida/javalivesummary/Stringz.java
@@ -0,0 +1,19 @@
+package com.edwaldoalmeida.javalivesummary;
+
+public class Stringz {
+    public static void main(String[] args) {
+        String logLine = "[WARNING]: Disk almost full";
+
+        // extract the log message
+        String message = logLine.substring(logLine.indexOf(":") + 1).trim();
+
+        // extract the log level
+        int openBracket = logLine.indexOf("[");
+        int closeBracket = logLine.indexOf("]");
+        String logLevel = logLine.substring(openBracket + 1, closeBracket);
+
+        // reformat log line
+        String reformatted = message + " (" + logLevel + ")";
+    }
+
+}
