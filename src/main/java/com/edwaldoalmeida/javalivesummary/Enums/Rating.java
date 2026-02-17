8143e45c134b9d85add0c4f15a42999c55592ca7
Edwaldo Almeida Jr <edwaldoalmeida@icloud.com>
2026-02-16 22:16:46 -0500

    adds Rating.java

---
+++ src/main/java/com/edwaldoalmeida/javalivesummary/Enums/Rating.java
@@ -0,0 +1,19 @@
+package com.edwaldoalmeida.javalivesummary.Enums;
+
+public enum Rating {
+    GREAT(5),
+    GOOD(4),
+    OK(3),
+    BAD(2),
+    TERRIBLE(1);
+
+    private final int numberOfStars;
+
+    Rating (int numberOfStars) {
+        this.numberOfStars = numberOfStars;
+    }
+
+    public int getNumberOfStars() {
+        return this.numberOfStars;
+    }
+}
