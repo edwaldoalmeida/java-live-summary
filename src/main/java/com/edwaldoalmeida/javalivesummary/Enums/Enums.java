12783dc445ad655eebd66e79e322b648821b5f44
Edwaldo Almeida Jr <edwaldoalmeida@icloud.com>
2026-02-16 21:50:41 -0500

    updates to Enums.java

--- src/main/java/com/edwaldoalmeida/javalivesummary/Enums/Enums.java
+++ src/main/java/com/edwaldoalmeida/javalivesummary/Enums/Enums.java
@@ -6,10 +6,19 @@ public class Enums {
     public static void main(String[] args) {
         System.out.println("Hello Enums!");
 
+        // DaysOfWeek
         Shop shop = new Shop();
         String openingHoursMonday = shop.getOpeningHours(DaysOfWeek.MONDAY);
         String openingHoursSaturday = shop.getOpeningHours(DaysOfWeek.SATURDAY);
         String openingHoursSunday = shop.getOpeningHours(DaysOfWeek.SUNDAY);
+
+        // Rating 
+        int rating = Rating.GOOD.getNumberOfStars();
+
+        // just experimenting
+        DaysOfWeek monday = DaysOfWeek.valueOf("MONDAY"); // returns the Object from the Enum
+        String mondayName = monday.name();
+        int mondayOrdinal = monday.ordinal();
 
         System.exit(1);
     }
