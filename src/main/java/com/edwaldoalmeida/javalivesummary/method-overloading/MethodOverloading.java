b7e74bd7c9cd2dc3394d5f13a661b4eb7b8baadf
Edwaldo Almeida Jr <edwaldoalmeida@icloud.com>
2026-02-20 22:06:46 -0500

    MethodOverloading.java

---
+++ src/main/java/com/edwaldoalmeida/javalivesummary/method-overloading/MethodOverloading.java
@@ -0,0 +1,23 @@
+package com.edwaldoalmeida.javalivesummary.method-overloading;
+
+/*
+# Method Overloading in Java
+    * feature that allows a class to have more than one method having the same name, if their parameter lists are different
+    * It is related to compile-time (or static) polymorphism
+    * The key to method overloading is a method's signature.
+    * Two methods will be considered different if they have different signatures.
+* There are two ways to overload a method:
+  - Different Number of Parameters
+  - Different Types of Parameters
+* Notes:
+  - Overloaded methods must change the argument list.
+  - Overloaded methods can also change the return type, but merely changing the return type does not constitute method overloading.
+  - Methods can be overloaded in the same class or in a subclass.
+*/
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+        System.out.println("Hello Method Overloading!");
+        // TODO: include demonstration of the use of Method Overloading here (after finishing the respective exercism exercise)
+    }
+}
