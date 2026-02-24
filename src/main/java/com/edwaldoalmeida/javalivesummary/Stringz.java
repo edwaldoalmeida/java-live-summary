782d6f909da4bdf48e3c188403aff6b9b788db1d
Edwaldo Almeida Jr <edwaldoalmeida@icloud.com>
2026-02-23 22:14:48 -0500

    updates Stringz.java

--- src/main/java/com/edwaldoalmeida/javalivesummary/Stringz.java
+++ src/main/java/com/edwaldoalmeida/javalivesummary/Stringz.java
@@ -13,11 +13,22 @@ public class Stringz {
         String logLevel = logLine.substring(openBracket + 1, closeBracket);
 
         // reformat log line
-        String reformatted = message + " (" + logLevel + ")";
+        String reformatted1 = message + " (" + logLevel + ")";
+
+        // reformat log line (another way)
+        String reformatted2 = String.format("%s (%s)", message, logLevel);
 
         // from int to String
         int i = 10;
         String iString = Integer.toString(i);
+
+        String myString = "hey";
+        int myInt = 10;
+        float myFloat = 1.23456789f;
+        Double myDouble = 1.23456789;
+        String reformatted3 = String.format("String: %s, int: %04d, Double: %f, Float: %f", myString, myInt, myDouble, myFloat);
+
+        System.exit(1);
     }
 
 }
